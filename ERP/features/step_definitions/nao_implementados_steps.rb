4d3f8e22a9bec656e10262fb25105f032d360dca
Leticia Trentini Lima <leticia.lima@vpsa.com.br>
2019-04-03 15:03:40 -0300

    Ajuste

--- ERP/features/step_definitions/nao_implementados_steps.rb
+++ ERP/features/step_definitions/nao_implementados_steps.rb
@@ -1,11 +1,8 @@
 
-  
   Quando("a soma do valor total dos itens é igual a {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
   end
 
   Quando("o total dos produtos na nota é igual a {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
   end
 
   Então("os centavos divergentes serão distribuídos entre os itens da nota") do
@@ -14,21 +11,16 @@
   
   
   Então("a fórmula para geração do custo do produto para a {string} será o valor total do item + {string} + {string}") do |string, string2, string3|
-    pending # Write code here that turns the phrase above into concrete actions
   end
   
   Então("o custo unitário é a divisão do custo total pela quantidade") do
-    pending # Write code here that turns the phrase above into concrete actions
   end
   
   Então("o item {int} de código {string} com quantidade {string} e valor total {string} utilizará a fórmula: {string} + {string} para chegar ao custo final {string}") do |int, string, string2, string3, string4, string5, string6|
-    pending # Write code here that turns the phrase above into concrete actions
   end
   
   Então("o item {string} com quantidade {string} e com valor total do produto {string} utilizará a fórmula: {string} + {string} para chegar ao custo final {string}") do |string, string2, string3, string4, string5, string6|
-    pending # Write code here that turns the phrase above into concrete actions
   end
   
   Então("o custo unitário é a divisão do custo do produto pela quantidade") do
-    pending # Write code here that turns the phrase above into concrete actions
   end
