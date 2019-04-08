68ef905fb55335fe29f16b775599d9c0666e2e0f
leticiatrentini <leticia.lima@vpsa.com.br>
2019-04-08 13:27:28 -0300

    Melhoria

--- PDV/src/test/java/steps/Hooks.java
+++ PDV/src/test/java/steps/Hooks.java
@@ -3,6 +3,7 @@ package steps;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import org.junit.AfterClass;
 import org.sikuli.script.FindFailed;
 
 import abrirModulos.AbrirManagerSincronismo;
@@ -20,7 +21,7 @@ public class Hooks {
 	private AfterTeste validaFinal = AfterTeste.getInstance();
 	private AbrirManagerSincronismo sincronismo = AbrirManagerSincronismo.getInstance();
 	static String docFiscalParametro;
-	
+
 	@Before
 	public static void prepararPDV() throws FindFailed, ClassNotFoundException, InterruptedException, IOException, SQLException{
 		docFiscalParametro = System.getProperty("docFiscal");
@@ -32,11 +33,14 @@ public class Hooks {
 		System.out.println("PDV configurado para:" + docFiscal);
 		verificaPDV.verificaSituacaoPDV(docFiscal);
 	} 
- 
- @After
- public void finalizando(Scenario scenario) throws ClassNotFoundException, FindFailed, InterruptedException, SQLException, IOException{
-		System.out.println(this.getClass().getName());
+
+	@After
+	public void finalizando(Scenario scenario) throws ClassNotFoundException, FindFailed, InterruptedException, SQLException, IOException{
 		validaFinal.validaExecucao(scenario, this.getClass().getName());
+	}
+
+	@AfterClass
+	public void depoisCenarios() throws ClassNotFoundException, FindFailed, InterruptedException, SQLException, IOException{
 		sincronismo.sincronizar();
 		statusPDV.fecharPDV();
 	}
