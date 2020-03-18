ab5ae64282bd0bdb0eb93c37c97f39eac0f5863d
CarlosLaurindo <52835928+CarlosLaurindo@users.noreply.github.com>
2020-03-18 20:10:25 -0300

    NOVO PDV CARLOS

--- NOVO_PDV/cypress/integration/Pre-vendaSpec.js
+++ NOVO_PDV/cypress/integration/Pre-vendaSpec.js
@@ -4,6 +4,7 @@ import inserirItens from '../support/elements/inserirItensElements'
 import checkout from '../support/elements/checkoutElements'
 import cliente from '../support/elements/clienteElements'
 import desconto from '../support/elements/descontoElements'
+import menu from '../support/elements/menuElements'
 
 describe('Pré-venda', () => {
 
@@ -39,12 +40,12 @@ describe('Pré-venda', () => {
         cy.get(inserirItens.BTN_SALVAR_ATENDIMENTO).click()
     })
 
-    it(`Pré venda para cliente válido`, () => {
-        cy.get(inserirItens.PRODUTO.FN_ADICIONAR_CARRINHO('1')).click()
-        cy.get(inserirItens.PRODUTO.MENSAGEM)
-                .should('contain', 'Produto BLUSA MANGA CURTA REF XYZ P AZ sem classificação fiscal')
-        cy.get(inserirItens.BTN_SALVAR_ATENDIMENTO).click()
-    })
+  //  it(`Pré venda para cliente válido`, () => {
+  //      cy.get(inserirItens.PRODUTO.FN_ADICIONAR_CARRINHO('1')).click()
+  //      cy.get(inserirItens.PRODUTO.MENSAGEM)
+  //              .should('contain', 'Produto BLUSA MANGA CURTA REF XYZ P AZ sem classificação fiscal')
+  //      cy.get(inserirItens.BTN_SALVAR_ATENDIMENTO).click()
+  //  })
 
    it(`Pré venda para cliente diferente de diversos`, () => {
     cy.get(inserirItens.PRODUTO.INSERCAORAPIDA).click()
@@ -61,7 +62,7 @@ describe('Pré-venda', () => {
     })
         
 
-    it.only(`Venda com vínculo de cliente`, () => {
+    it(`Venda com vínculo de cliente`, () => {
         cy.get(cliente.CLIENTE.CLIENTE).contains('Diversos').click()
         cy.get(cliente.CLIENTE.INSERCAOCLIENTE).type('Carlos Alessandro Laurindo')
         cy.contains('095.605.299-19').click()
@@ -74,17 +75,27 @@ describe('Pré-venda', () => {
     })
      
 
-    it(`Inserção de desconto`, () => {
-        cy.get(inserirItens.PRODUTO.INSERCAORAPIDA).click()
-        cy.get(inserirItens.PRODUTO.ADICIONARITEM).type('2*Boneca T02.')
-        cy.get(checkout.CHECKOUT.DESCRICAOPRODUTO)
-        .should('contain', 'Boneca T02.')
-        //cy.get('.mb-3',).type('50,00')
-        cy.get('.flex-direction-column')
-        cy.get('.mb-3',)
-        cy.get('.form-control').type('50,00')
-    })
+  //  it(`Inserção de desconto`, () => {
+  //      cy.get(inserirItens.PRODUTO.INSERCAORAPIDA).click()
+  //      cy.get(inserirItens.PRODUTO.ADICIONARITEM).type('2*Boneca T02.')
+  //      cy.get(checkout.CHECKOUT.DESCRICAOPRODUTO)
+  //      .should('contain', 'Boneca T02.')
+  //      //cy.get('.mb-3',).type('50,00')
+  //      cy.get('.flex-direction-column')
+  //      cy.get('.mb-3',)
+  //      cy.get('.form-control').type('50,00')
+  //  })
      
-
+  it.only(`Relatório de movimentações`, () => {
+    cy.get(menu.SELECAO.MOVIMENTACOES).click()
+    cy.get(':nth-child(1) > .card-body > .clickable > .fad').click()
+    cy.get('[tooltip="Continuar atendimento"] > .acao-atendimento > .acao-legenda-completa').click()
+    cy.get(':nth-child(1) > .w-25 > .botoesQuantidade > :nth-child(3) > .fal').click()
+    cy.get(':nth-child(1) > .w-50 > span').click()
+    cy.get('.select2-selection__arrow > b').click()
+    cy.get('.select2-search__field').type('GERENTE')
+    //cy.get(':nth-child(1) > .w-50 > span').click()
+    cy.get('.cdk-overlay-backdrop').click()
+})
 
 })
---
+++ NOVO_PDV/cypress/support/elements/menuElements.js
@@ -0,0 +1,14 @@
+const menuElements = {
+    SELECAO:{
+        ATENDIMENTO: '.fa-shopping-cart',
+        MOVIMENTACOES: '.fa-file-chart-line',
+        RANKINGVENDEDOR: '.fa-chart-bar',
+        RELCLIENTES: '.fa-users',
+        PESQUISAITEM: '.fa-search',
+        CONFIGURACAO: '.fa-cog',
+        TRANSACAOPENDENTE: '.fa-credit-card'
+    },
+
+}
+
+export default menuElements;
